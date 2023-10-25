809620af3d00c90b905cf80170cd9bb1ae22e3a6
lcm205 <146500405+lcm205@users.noreply.github.com>
2023-10-25 20:14:26 +0900

    Add files via upload

---
+++ 003 Code/완성 코드/Flutter/API.dart
@@ -0,0 +1,7 @@
+class API {
+  static const hostconnect = "http://192.168.0.40/testerhoon";
+  static const tester = "http://3.144.225.107/connection.php";
+  static const getname = "http://3.144.225.107/getname.php";
+  static const local_2img_gpt = "http://3.144.225.107/local_2img_gpt.php";
+  static const timedata = "http://3.144.225.107/testdata.php";
+}
